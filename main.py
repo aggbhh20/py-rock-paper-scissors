317f7b520be2783116c221e136be1eaa84ffba04
aggbhh20 <105166213+aggbhh20@users.noreply.github.com>
2022-12-04 21:51:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import random
-options = ["rock", "paper", "scissors"]
+options = ["rock", "paper", "scissors"] #defining our options
 def checkInput(usrInput):
     if choice == usrInput:
+        #in case of draw
         print("pc picked: " + options[choice - 1])
         print("DRAW")
     elif usrInput - choice == -2:
+        #rock(1) beats scissors(3) so rock-scissors = -2
         print("pc picked: " + options[choice - 1])
         print("you win!")
     elif usrInput - choice == 1:
+        #paper(2) beats rock(1) so paper - rock = 1, scissors(3) beats paper(2) so scissors - paper = 1
         print("pc picked: " + options[choice - 1])
         print("you win!")
     elif usrInput > 3:
+        #if the player enters a number bigger than 3
         print("PICK A NUMBER BETWEEN 1 AND 3!")
     else:
+        #if the player does not meet the conditions to win
         print("pc picked: " + options[choice - 1])
         print("you lose!")
 while (True):
